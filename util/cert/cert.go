a8ecd263f89ef734cd55a024e05deb6f8c11926f
josexy <josephxrays@163.com>
2024-06-15 17:16:35 +0800

    update

--- util/cert/cert.go
+++ util/cert/cert.go
@@ -8,14 +8,17 @@ import (
 )
 
 func GetClientMTlsConfig(certPath, keyPath, caPath, hostname string) (*tls.Config, error) {
-	ca, err := os.ReadFile(caPath)
-	if err != nil {
-		return nil, err
-	}
-	// client ca file
-	certPool := x509.NewCertPool()
-	if ok := certPool.AppendCertsFromPEM(ca); !ok {
-		return nil, errors.New("failed to append certs")
+	var rootCAs *x509.CertPool
+	if caPath != "" {
+		ca, err := os.ReadFile(caPath)
+		if err != nil {
+			return nil, err
+		}
+		// client ca file
+		rootCAs = x509.NewCertPool()
+		if ok := rootCAs.AppendCertsFromPEM(ca); !ok {
+			return nil, errors.New("failed to append certs")
+		}
 	}
 	// client mtls cert and key file
 	cert, err := tls.LoadX509KeyPair(certPath, keyPath)
@@ -25,19 +28,22 @@ func GetClientMTlsConfig(certPath, keyPath, caPath, hostname string) (*tls.Confi
 	return &tls.Config{
 		Certificates: []tls.Certificate{cert}, // used for client tls
 		ServerName:   hostname,                // ussed for client common tls
-		RootCAs:      certPool,                // used for client common tls
+		RootCAs:      rootCAs,                 // used for client common tls
 	}, nil
 }
 
 func GetServerMTlsConfig(certPath, keyPath, caPath string) (*tls.Config, error) {
-	// server ca file
-	ca, err := os.ReadFile(caPath)
-	if err != nil {
-		return nil, err
-	}
-	certPool := x509.NewCertPool()
-	if ok := certPool.AppendCertsFromPEM(ca); !ok {
-		return nil, errors.New("failed to append ca file to cert pool")
+	var clientCAs *x509.CertPool
+	if caPath != "" {
+		// server ca file
+		ca, err := os.ReadFile(caPath)
+		if err != nil {
+			return nil, err
+		}
+		clientCAs = x509.NewCertPool()
+		if ok := clientCAs.AppendCertsFromPEM(ca); !ok {
+			return nil, errors.New("failed to append ca file to cert pool")
+		}
 	}
 	// server tls cert and key file
 	cert, err := tls.LoadX509KeyPair(certPath, keyPath)
@@ -47,7 +53,7 @@ func GetServerMTlsConfig(certPath, keyPath, caPath string) (*tls.Config, error) 
 	return &tls.Config{
 		Certificates: []tls.Certificate{cert},        // used for server tls
 		ClientAuth:   tls.RequireAndVerifyClientCert, // used for server mtls
-		ClientCAs:    certPool,                       // used for server mtls
+		ClientCAs:    clientCAs,                      // used for server mtls
 	}, nil
 }
 
@@ -59,17 +65,20 @@ func GetInsecureTlsConfig() *tls.Config {
 
 // client tls need ca file and Common name
 func GetClientTlsConfig(caPath, hostname string) (*tls.Config, error) {
-	ca, err := os.ReadFile(caPath)
-	if err != nil {
-		return nil, err
-	}
-	certPool := x509.NewCertPool()
-	if ok := certPool.AppendCertsFromPEM(ca); !ok {
-		return nil, errors.New("failed to append ca file to cert pool")
+	var rootCAs *x509.CertPool
+	if caPath != "" {
+		ca, err := os.ReadFile(caPath)
+		if err != nil {
+			return nil, err
+		}
+		rootCAs = x509.NewCertPool()
+		if ok := rootCAs.AppendCertsFromPEM(ca); !ok {
+			return nil, errors.New("failed to append ca file to cert pool")
+		}
 	}
 	return &tls.Config{
 		ServerName: hostname,
-		RootCAs:    certPool,
+		RootCAs:    rootCAs,
 	}, nil
 }
 
